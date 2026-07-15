5182fcc1e0c0f223ca0d41db79a8ed9d222fba93
DrSkyeagle <a1638614144@outlook.com>
2026-07-15 12:58:40 +0800

    Add app/src/main/java/com/floatingclock/MainActivity.kt

---
+++ app/src/main/java/com/floatingclock/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.floatingclock
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.provider.Settings
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+            !Settings.canDrawOverlays(this)
+        ) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivity(intent)
+        }
+
+        startService(Intent(this, FloatingService::class.java))
+        finish()
+    }
+}
